25b6707e19d291068cd9281ca7e1a57de06c8b94
eaCe <webmaster@slaps.de>
2023-07-02 16:54:17 +0200

    feat: adds mix directive

--- lib/directives/helpers.php
+++ lib/directives/helpers.php
@@ -98,4 +98,31 @@ return [
     'vardump' => static function ($vars) {
         return "<?php echo '<pre>'; var_dump({$vars}); echo '</pre>' ?>";
     },
+
+    /**
+     * mix, eg. for TAR.
+     */
+    'mix' => static function ($path) {
+        $manifestPath = rex_path::frontend('dist/mix-manifest.json');
+
+        static $manifest;
+
+        if (!$manifest) {
+            if (!file_exists($manifestPath)) {
+                throw new Exception('The Mix manifest does not exist.');
+            }
+
+            $manifest = json_decode(file_get_contents($manifestPath), true, 512, JSON_THROW_ON_ERROR);
+        }
+
+        if (!str_starts_with($path, '/')) {
+            $path = "/{$path}";
+        }
+
+        if (!array_key_exists($path, $manifest)) {
+            throw new Exception("Unable to locate Mix file: {$path}. Please check your " . 'webpack.mix.js output paths and try again.');
+        }
+
+        return rex_url::frontend('dist'.$manifest[$path]);
+    },
 ];
